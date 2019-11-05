01cf941d2d876ff77671cee1efc891f1aeb0c0ae
Cedric B <cedricbardaine@gmail.com>
2019-11-05 16:40:45 +0100

    First version that works

--- public/client.js
+++ public/client.js
@@ -20,5 +20,64 @@ $('#chat form').submit(function (e) {
  * Réception d'un message
  */
 socket.on('chat-message', function (message) {
-  $('#messages').append($('<li>').text(message.text));
+  $('#messages').append($('<li>').html('<span class="username" style="background:'+message.usercolor+';"> ' + message.username + '</span>' + message.text) ) ;
+  scrollToBottom() ; 
+});
+
+/**
+ * User connection :
+ */
+$('#login form').submit(function(e) {
+  e.preventDefault() ; 
+  var user = {
+    username : $('#login input').val().trim(),
+    usercolor : $('#idColor').val()
+  }; 
+  if (user.username.length > 0) {
+    socket.emit('user-login', user, function (success) {
+      if (success) {
+        $('body').removeAttr('id') ; 
+        $('#chat input').focus() ;
+      }
+    }) ; 
+  }
+})
+
+/**
+ * Réception d'un msg
+ */
+socket.on('service-message' , function( message) {
+  $('#messages').append( $( '<li class="'+message.type+'">' ).html('<span class="info">' + message.text + '</span>') ) ; 
+  scrollToBottom() ; 
+})
+
+
+/**
+ * Scroll vers le bas de page si l'utilisateur n'est pas remonté pour lire d'anciens messages
+ */
+function scrollToBottom() {
+  if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
+    $("html, body").animate({ scrollTop: $(document).height() }, 0);
+  }
+}
+
+
+
+
+/**
+ * Connexion d'un nouvel utilisateur
+ */
+socket.on('user-login', function (user) {
+  $('#users').append($('<li class="' + user.username + ' new" style="border: 3px solid '+user.usercolor+'">').html(user.username) ) ;
+  setTimeout(function () {
+    $('#users li.new').removeClass('new');
+  }, 1000);
+});
+
+/**
+ * Déconnexion d'un utilisateur
+ */
+socket.on('user-logout', function (user) {
+  var selector = '#users li.' + user.username;
+  $(selector).remove();
 });
--- server.js
+++ server.js
@@ -3,32 +3,98 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [] ; 
+
 /**
- * Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
- */
+* Gestion des requêtes HTTP des utilisateurs en leur renvoyant les fichiers du dossier 'public'
+*/
 app.use("/", express.static(__dirname + "/public"));
 
 io.on('connection', function (socket) {
-
+  var user ; 
+  
   /**
-   * Log de connexion et de déconnexion des utilisateurs
-   */
+  * Connexion d'un utilisateur via le formulaire
+  */
+  socket.on('user-login', function (loggedUser, callback) {
+    var userIndex = -1 ; 
+    for (let i = 0; i < users.length; i++) {
+      if( users[i].username === loggedUser.username) {
+        userIndex = i ; 
+      }
+    }
+    /* Emission d'un événement "user-login" pour chaque utilisateur connecté */ 
+    for (let i = 0; i < users.length; i++) { socket.emit('user-login' ,  users[i]) ; }
+    
+    if(loggedUser !== undefined && userIndex === -1) {
+      user = loggedUser;
+      users.push(user) ; 
+      console.log('user logged in : ' + loggedUser.username);
+      
+      var userServiceMessage = {
+        text: 'You logged in as : '+user.username ,
+        type: 'login'
+      };
+      var broadcastServiceMessage = {
+        text: 'User : '+user.username+' logged in',
+        type: 'login'
+      };
+      socket.emit('service-message', userServiceMessage) ; 
+      socket.broadcast.emit('service-message', broadcastServiceMessage) ; 
+      
+      io.emit('user-login', user) ; 
+      callback(true); 
+
+      //// unused
+      // var serviceMessage = {
+      //   text: 'User : '+ user.username + ' logged in.' ,
+      //   type: 'login'
+      // };
+      // socket.broadcast.emit('service-message', serviceMessage) ; 
+
+    }
+    else {
+      callback(false) ; 
+    }
+    
+    
+  });
+  
+  /**
+  * Log de connexion et de déconnexion des utilisateurs
+  */
   console.log('a user connected');
   socket.on('disconnect', function () {
-    console.log('user disconected');
+    if( user !== undefined){
+      console.log('user disconected : ' + user.username);
+      var serviceMessage = {
+        text: 'User : ' + user.username + ' disconnected.',
+        type: 'logout' 
+      }; 
+      socket.broadcast.emit('service-message', serviceMessage) ;
+      
+      var userIndex = users.indexOf(user) ; 
+      if ( userIndex !== -1) {
+        users.splice(userIndex, 1 ) ; 
+      }
+      io.emit('user-logout', user) ; 
+    }
   });
-
+  
   /**
-   * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
-   */
+  * Réception de l'événement 'chat-message' et réémission vers tous les utilisateurs
+  */
   socket.on('chat-message', function (message) {
+    message.username = user.username; // On intègre ici le nom d'utilisateur au message
+    message.usercolor = user.usercolor;
     io.emit('chat-message', message);
+    console.log('Message de : ' + user.username);
   });
 });
 
 /**
- * Lancement du serveur en écoutant les connexions arrivant sur le port 3000
- */
+* Lancement du serveur en écoutant les connexions arrivant sur le port 3000
+*/
 http.listen(3000, function () {
-  console.log('Server is listening on *:3000');
+  console.log('Server is listening on localhost:3000');
 });
